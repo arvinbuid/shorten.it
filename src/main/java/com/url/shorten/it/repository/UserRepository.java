736d5a8b90da91e33405622687ca038e7f1ff648
Arvin Buid <arvinbuid01@gmail.com>
2025-07-19 17:49:53 +0800

    feat: add user repository and service for authentication

---
+++ src/main/java/com/url/shorten/it/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.url.shorten.it.repository;
+
+import com.url.shorten.it.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+ }
