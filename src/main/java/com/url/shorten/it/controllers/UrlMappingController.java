46fb65affff0a78e15765d9aa2d736a4f3988bf3
Arvin Buid <arvinbuid01@gmail.com>
2025-07-21 16:14:36 +0800

    feat: add endpoint and create GET method inside UrlMappingController class to get all user-specific URLs

--- src/main/java/com/url/shorten/it/controllers/UrlMappingController.java
+++ src/main/java/com/url/shorten/it/controllers/UrlMappingController.java
@@ -10,6 +10,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -29,5 +30,13 @@ public class UrlMappingController {
         User user = userService.findByUsername(principal.getName());
         UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl, user);
         return ResponseEntity.ok(urlMappingDTO);
+    }
+
+    @GetMapping("/myurls")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<UrlMappingDTO>> getUserUrls(Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        List<UrlMappingDTO> urls = urlMappingService.getUrlsByUser(user);
+        return ResponseEntity.ok(urls);
     }
 }
