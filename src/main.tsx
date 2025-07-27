952026d6757e45029b91dedfdcfa27f8d4c8edfd
Arvin Buid <arvinbuid01@gmail.com>
2025-07-27 21:06:41 +0800

    feat(deps): integrate TanStack Query and Devtools for server state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { JwtProvider } from './context/JwtContext.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import './index.css'
 import App from './App.tsx'
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <JwtProvider>
-      <App />
-    </JwtProvider>
+    <QueryClientProvider client={queryClient}>
+      <JwtProvider>
+        <App />
+      </JwtProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 )
